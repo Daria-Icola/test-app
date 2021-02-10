bbc0a86e913b9b76699f2e1bbd79b6a36c0fac90
Daria-Icola <42102070+Daria-Icola@users.noreply.github.com>
2021-02-10 17:36:40 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,22 +8,16 @@ import { store } from './components/redux/redux-store';
 import reportWebVitals from './reportWebVitals';
 
 
-// let rerenderTree = (state) => {
-    ReactDOM.render(
-        <BrowserRouter>
-            <Provider store={store}>
-                <App />
-            </Provider>
-        </BrowserRouter>,
-        document.getElementById('root')
-    )
 
+ReactDOM.render(
+    <BrowserRouter>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </BrowserRouter>,
+    document.getElementById('root')
+)
 
-// rerenderTree(store.getState());
-// store.subsctibe(() => {
-//     let state = store.getState()
-//     rerenderTree(state)
-// });
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
